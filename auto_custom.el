0f12755c1023d4d2d3954ee4148c5ba154ed5e90
GeordieP <geordie.powers@gmail.com>
2018-01-08 22:48:20 -0500

    More cleaning up, regenerated and moved some things from custom file into config.org

--- auto_custom.el
+++ auto_custom.el
@@ -1,8 +1,3 @@
-;;
-;; custom file
-;; emacs will use this file as a place to save its auto-generated config
-;; don't modify anything in here unless you know what you're doing
-;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -10,19 +5,4 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (autothemer fish-mode org-bullets rjsx-mode ivy powerline evil rainbow-delimiters highlight-indent-guides which-key use-package hl-todo general evil-escape evil-commentary)))
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:box nil :overline nil :underline nil :weight bold))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
+    (autothemer fish-mode rjsx-mode rust-mode rainbow-delimiters hl-todo company counsel-projectile projectile counsel flx ivy org-bullets evil-org evil-commentary evil-escape evil which-key general use-package))))
