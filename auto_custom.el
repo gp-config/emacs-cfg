79579a98bcd7dbf2f8ad952d88b8a440f486dbac
GeordieP <geordie.powers@gmail.com>
2018-01-12 09:54:23 -0500

    Add magit, remove custom file from repo

--- auto_custom.el
+++
@@ -1,8 +0,0 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (autothemer fish-mode rjsx-mode rust-mode rainbow-delimiters hl-todo company counsel-projectile projectile counsel flx ivy org-bullets evil-org evil-commentary evil-escape evil which-key general use-package))))
